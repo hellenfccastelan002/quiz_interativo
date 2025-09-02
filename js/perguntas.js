f96b69efb253aeebeebbc541839f3353c5698a15
HELLEN <hellen.castelan@escola.pr.gov.br>
2025-09-02 10:50:59 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,98 @@
+const perguntas = [
+    {
+        enunciado: "pergunta 1", //a pergunta
+        alternativas: [
+            {
+                texto: "nike", //primeira alternativa
+                afirmaçao: [
+                    "voce tem bom gosto", //comentario para a resposta
+                    "afirmaçao 2"         //segundo comentario
+                ],
+            },
+            {
+                texto: "adidas",
+                afirmaçao: [
+                    "voce e vaidado",
+                    "voce nao sabe escolher"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 2", 
+        alternativas: [
+            {
+                texto: "nike", 
+                afirmaçao: [
+                    "voce tem bom gosto", 
+                    "afirmaçao 2"         
+                ],
+            },
+            {
+                texto: "adidas",
+                afirmaçao: [
+                    "voce e vaidado",
+                    "voce nao sabe escolher"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 4", 
+        alternativas: [
+            {
+                texto: "nike", 
+                afirmaçao: [
+                    "voce tem bom gosto", 
+                    "afirmaçao 2"         
+                ],
+            },
+            {
+                texto: "adidas",
+                afirmaçao: [
+                    "voce e vaidado",
+                    "voce nao sabe escolher"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "voce prefere nike ou adidas", 
+        alternativas: [
+            {
+                texto: "nike", 
+                afirmaçao: [
+                    "voce tem bom gosto", 
+                    "afirmaçao 2"         
+                ],
+            },
+            {
+                texto: "adidas",
+                afirmaçao: [
+                    "voce e vaidado",
+                    "voce nao sabe escolher"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 5", 
+        alternativas: [
+            {
+                texto: "nike", 
+                afirmaçao: [
+                    "voce tem bom gosto", 
+                    "afirmaçao 2"         
+                ],
+            },
+            {
+                texto: "adidas",
+                afirmaçao: [
+                    "voce e vaidado",
+                    "voce nao sabe escolher"
+                ],
+            }
+        ]
+    },
+]
 
