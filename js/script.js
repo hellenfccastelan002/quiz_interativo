1c2577e0ec5e407df19d8dd3f5f8cdc0b5f317bc
HELLEN <hellen.castelan@escola.pr.gov.br>
2025-09-03 09:16:58 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './perguntas.js';
 
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const botaoIniciar = document.querySelector(".iniciar-bnt");
+const telaInicial = document.querySelector(".tela-inicial");
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo() {
+    atual = 0;
+    telaInicial.style.display = 'none';
+    caixaPerguntas.cassList.remove(".mostrar");
+    caixaAlternativas.classList.remove(".mostrar");
+    caixaResultado.classList.remove(".mostrar")
+    mostraPergunta();
+}
+
+function mostraPergunta() {
+    if(atual <= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {}
+
+function mostraResultado() {
+    caixaPerguntas.textContent =  `Apos tudo isso, ${nome} descobriu que`;
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+    caixaResultado.classList.add
+}
